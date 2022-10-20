e34670fe9fd5402e27e7372f0c10af41d86300c4
rohit <rhtry.tech@gmail.com>
2022-10-20 23:51:34 +0530

    adding files

---
+++ client/src/Components/index.js
@@ -0,0 +1,39 @@
+export { default as PrimaryHome } from "./PrimaryHome.jsx";
+
+export { default as Home } from "./Home.jsx";
+
+export { default as Navbar } from "./Navbar.jsx";
+
+export { default as Footer } from "./Footer.jsx";
+
+// Login Exports
+
+export { default as IndividualLogin } from "./Individual/IndividualLogin.jsx";
+
+export { default as InstitutionLogin } from "./Institution/InstitutionLogin.jsx";
+
+// Profile Exports
+
+export { default as IndividualProfile } from "./Individual/IndividualProfile";
+
+export { default as InstitutionProfile } from "./Institution/InstitutionProfile";
+
+// Further Routes
+
+export { default as OrganiseAnEvent } from "./Institution/OrganiseAnEvent";
+
+export { default as JoinAnEvent } from "./Individual/JoinAnEvent.jsx";
+
+export { default as SendFoodSupplies } from "./Individual/SendFoodSupplies";
+
+export { default as RequestFoodStock } from "./Institution/RequestFoodStock";
+
+export { default as ListOfInstitution } from "./ListOfInstitution";
+
+export { default as RegisterAsIndividual } from "./Individual/Register";
+
+export { default as RegisterAsInstitution } from "./Institution/Register";
+
+export { default as AboutUs } from "./AboutUs";
+
+export { default as Contact } from "./Contact";
