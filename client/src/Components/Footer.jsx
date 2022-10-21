7011ebb52f21ffb57ad4b95d25df4332dca4785c
rohit <rhtry.tech@gmail.com>
2022-10-21 23:58:35 +0530

    adding new files !

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "../Styles/footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <h3>Mission Zero Hunger</h3>
+        <h3>Rohit Roy | 2022</h3>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
